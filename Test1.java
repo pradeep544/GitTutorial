a97074ba778eebf81accee7b83dac9c43901f4a3
Pradeep,Kukkkala <pradeepprdp58@gmail.com>
2017-11-01 00:20:45 +0530

    committing first file from java folder

---
+++ Test1.java
@@ -0,0 +1,18 @@
+import static java.lang.System.*;
+
+class Test1
+{
+	static void helo1(){
+		out.println("hello1");	
+	}
+	public static void main(String[] args ){
+		int a =10;
+		out.println("Static import test");
+		//hello();
+	}
+	
+	void hello(){
+		out.println("hello");
+	}
+	
+}
